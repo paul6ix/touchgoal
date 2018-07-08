5366256f2b679d2146a066311ecb492831be5627
paul6ix <okporp@gmail.com>
2018-07-08 23:15:18 +0100

    added remote database

--- data/db.js
+++ data/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 dbhost = 'mongodb://localhost:27017/taskDB';
-mongoose.connect(dbhost, {
+remotehost = 'mongodb://paul:task123@ds229771.mlab.com:29771/tasks-db';
+mongoose.connect(remotehost, {
 }).then(function () {
     console.log('Connected to database ' );
 
