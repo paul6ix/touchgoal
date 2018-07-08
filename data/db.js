cae5c36d8cc8f6f06b6a4414f03d71a1433ed206
paul6ix <okporp@gmail.com>
2018-07-09 00:30:54 +0100

    fixed mongodb error issues

--- data/db.js
+++ data/db.js
@@ -4,6 +4,7 @@ mongoose.Promise = global.Promise;
 dbhost = 'mongodb://localhost:27017/taskDB';
 remotehost = 'mongodb://paul:task123@ds229771.mlab.com:29771/tasks-db';
 mongoose.connect(remotehost, {
+    useNewUrlParser:true
 }).then(function () {
     console.log('Connected to database ' );
 
