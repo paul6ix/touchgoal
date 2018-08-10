68d505a2124dc5c7728a295b92cdffb184725a19
paul6ix <okporp@gmail.com>
2018-08-10 09:13:26 +0100

    added chinedu email for report

--- data/db.js
+++ data/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 dbhost = 'mongodb://localhost:27017/taskDB';
 remotehost = 'mongodb://paul:task123@ds229771.mlab.com:29771/tasks-db';
-mongoose.connect(dbhost, {
+mongoose.connect(remotehost, {
     useNewUrlParser:true
 }).then(function () {
     console.log('Connected to database ' );
